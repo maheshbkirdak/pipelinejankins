261d7d4a7071293417aaf92ca9c11f49159dcd6c
maheshbkirdak <maheshbkirdak@gmail.com>
2023-12-24 21:33:25 +0530

    first commit

---
+++ src/main/java/com/docker/api/PipelinejankinsApplication.java
@@ -0,0 +1,18 @@
+package com.docker.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+@SpringBootApplication
+public class PipelinejankinsApplication {
+	@GetMapping("/show")
+	public String getString() {
+		return "38.80 to 30  new year new place!!";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(PipelinejankinsApplication.class, args);
+	}
+
+}
